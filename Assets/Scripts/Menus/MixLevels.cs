8839578f271411eb07e88065180f01bdcd055e7e
Kirsten Love <kirstenlove208@gmail.com>
2024-03-17 18:46:50 -0400

    small volume slider changes

--- Assets/Scripts/Menus/MixLevels.cs
+++ Assets/Scripts/Menus/MixLevels.cs
@@ -8,6 +8,28 @@ public class MixLevels : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
     public AudioMixer masterMixer;
+    public int setSlider;
+
+    void Start()
+    {
+        float value = 0;
+        switch (setSlider)
+        {
+            case (1):
+                masterMixer.GetFloat("MasterVol", out value);
+                break;
+            case (2):
+                masterMixer.GetFloat("MusicVol", out value);
+                break;
+            case (3):
+                masterMixer.GetFloat("SFXVol", out value);
+                break;
+            default:
+                masterMixer.GetFloat("MasterVol", out value);
+                break;
+        }
+        volumeSlider.value = value;
+    }
 
     public void SetMasterLevel()
     {
