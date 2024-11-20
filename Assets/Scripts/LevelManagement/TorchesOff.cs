33d4ce7827c48e38a8d59d4c0602efb07e4a7882
Eishiii <arhulka@mtu.edu>
2024-11-19 22:20:16 -0500

    Temporary Lighting
    I do not understand lighting still, after hours of testing and such. So for the Playtest I will temporarily return to my original idea of just carrying around a light. Hopefully interactive lighting can be added next Semester. I did get the script working for interactive lighting, I just need a better understanding of light mapping.

---
+++ Assets/Scripts/LevelManagement/TorchesOff.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchesOff : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
