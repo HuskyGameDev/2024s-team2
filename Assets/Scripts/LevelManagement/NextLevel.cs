3c5554d45dabd4d965a8a33c476874ff2d90b2d0
Kirsten Love <kirstenlove208@gmail.com>
2025-02-16 22:32:22 -0500

    changed end level door to a portal
    potentially a temporary change, the door seemed to confused some players last semester

--- Assets/Scripts/LevelManagement/NextLevel.cs
+++ Assets/Scripts/LevelManagement/NextLevel.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    public Transform player;
+    private Vector3 offset = new Vector3(0, 180, 0);
+
+    void Update()
+    {
+        transform.LookAt(player);
+        transform.Rotate(offset);
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
