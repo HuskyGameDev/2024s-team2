af5a7a380ff6babecc66e22cd793675fa576a41d
Eishiii <arhulka@mtu.edu>
2025-03-20 21:30:20 -0400

    Lvl 2 Update
    Added some new decorations and removed old ones. Added the SFX for the interactive torches lighting up.

--- Assets/Scripts/LevelManagement/TorchOn.cs
+++ Assets/Scripts/LevelManagement/TorchOn.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class TorchOn : MonoBehaviour, IInteractable
 {
     public GameObject obj;
+    public AudioSource audioSource;
+    public AudioClip[] TorchSounds;
     public void Start()
     {
         obj.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     public void Interact() {
-        if (obj.CompareTag("Torch")) {
+        if (obj.CompareTag("Torch") && (obj.transform.GetChild(0).gameObject.activeSelf == false)) {
+            audioSource.PlayOneShot(TorchSounds[UnityEngine.Random.Range(0, TorchSounds.Length - 1)]);
             obj.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
